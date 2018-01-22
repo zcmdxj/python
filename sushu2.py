95c759633933d2618ead7297831046fd80e03968
zcmdxj <mail66999@163.com>
2018-01-22 02:33:08 -0500

    add version

--- sushu2.py
+++ sushu2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+#version1
 for i in range(2,21):
     temp = 0
     for j in range(2,i):
@@ -10,4 +11,29 @@ for i in range(2,21):
     if temp == 1:
         print (i)
 
- 
+
+#version2
+for i in range(2,21):
+    temp = 0
+    x = int(i/2)
+    for j in range(2,x+1):
+        if i%x == 0:
+            break
+        else:
+            temp = 1
+    if temp == 1:
+        print(i)
+
+
+#version3
+import math
+for i in range(2,21):
+    temp = 0
+    x = int(math.sqrt(i))
+    for j in (2,x+1):
+        if i%j == 0:
+            break
+        else:
+            temp = 1
+    if temp == 1:
+        print(i)
